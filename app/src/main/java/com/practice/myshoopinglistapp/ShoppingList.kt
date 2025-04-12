8c1d2f7b14c7d04e9ab6f425940469c92ae446fe
LoopMaster99 <bansalkrishna909@gmail.com>
2025-04-13 03:01:02 +0530

    inital commmit

---
+++ app/src/main/java/com/practice/myshoopinglistapp/ShoppingList.kt
@@ -0,0 +1,148 @@
+package com.practice.myshoopinglistapp
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.practice.myshoopinglistapp.ui.theme.MyShoopingListAppTheme
+
+
+data class ShoppingItem(val id: Int,
+                        var name: String,
+                        var quantity: Int,
+                        var isEditing: Boolean = false
+)
+
+@Composable
+fun ShoppingList(){
+    var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
+    var showDialog by remember { mutableStateOf(false) }
+    var itemName by remember { mutableStateOf("") }
+    var itemQuantity by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { showDialog = true },
+            modifier = Modifier
+                .padding(top = 40.dp)
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Text("Add Item")
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            items(sItems){
+                ShoppingListItem(it, {}, {})
+            }
+        }
+    }
+
+    if(showDialog){
+        AlertDialog(onDismissRequest = { showDialog = false},
+            confirmButton = {
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Button(onClick = {
+                        if(itemName.isNotBlank()){
+                            val newItem = ShoppingItem(
+                                id = sItems.size+1,
+                                name = itemName,
+                                quantity = itemQuantity.toInt()
+                            )
+                            sItems += newItem
+                            showDialog = false
+                            itemName = "" // Clear the input fields for next time when we click on add item
+                        }
+                    }) {
+                        Text("Add")
+                    }
+                    Button(onClick = { showDialog = false }) {
+                        Text("Cancel")
+                    }
+                }
+            },
+            title = { Text("Add Shopping Item")},
+            text = {
+                Column(
+                    modifier = Modifier
+                ) {
+                    OutlinedTextField(
+                        value = itemName,
+                        onValueChange = { itemName = it },
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+
+                    Spacer(modifier = Modifier.padding(2.dp))
+
+                    OutlinedTextField(
+                        value = itemQuantity,
+                        onValueChange = { itemQuantity = it },
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit
+) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)
+        .border(border = BorderStroke(2.dp, Color.Gray), shape = RoundedCornerShape(10.dp))
+    ) {
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
+        Text(text = item.quantity.toString())
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ListPreview() {
+    MyShoopingListAppTheme {
+        ShoppingList()
+    }
+}
